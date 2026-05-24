c3299c37cb982b4bb05a93bb4d5bd39d4cd62120
mivalt.filip <mivalt.filip@gmail.com>
2026-05-23 23:53:04 -0500

    conf update

--- docs_src/source/conf.py
+++ docs_src/source/conf.py
@@ -7,3 +7,10 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 html_theme = 'sphinx_book_theme'
+
+html_js_files = [
+    ('https://bnelair.github.io/pagefind/pagefind-ui.js', {'type': 'text/javascript'}),
+]
+html_css_files = [
+    'https://bnelair.github.io/pagefind/pagefind-ui.css',
+]
